08d380357fb723d467519369d517f8f6bcef6042
Nafis-Fahim50 <nafisf96@gmail.com>
2022-11-24 11:52:05 +0600

    added react query

---
+++ src/Pages/Home/Categories/Categories.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Categories = () => {
+    
+    return (
+        <div className='mt-5'>
+            <h1 className='text-3xl font-bold text-center'>Browse items by category</h1>
+        </div>
+    );
+};
+
+export default Categories;
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import Categories from '../Categories/Categories';
 import Delivary from '../Delivary/Delivary';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <Categories></Categories>
             <Delivary></Delivary>
         </div>
     );
