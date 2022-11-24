d84e0a64640aa0143761dba30d37f374dc32bb87
Nafis-Fahim50 <nafisf96@gmail.com>
2022-11-24 12:17:16 +0600

    display categories

--- src/Pages/Home/Categories/Categories.js
+++ src/Pages/Home/Categories/Categories.js
@@ -1,10 +1,28 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import Category from './Category';
 
 const Categories = () => {
-    
+    const {data: categories = []} = useQuery({
+        queryKey: ['categories'],
+        queryFn: async() =>{
+            const res = await fetch('http://localhost:5000/categories');
+            const data = await res.json();
+            return data;
+        }
+    })
     return (
         <div className='mt-5'>
             <h1 className='text-3xl font-bold text-center'>Browse items by category</h1>
+
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-5'>
+                {
+                    categories.map(category=> <Category
+                    key={category._id}
+                    category={category}></Category>)
+                }
+            </div>
+        
         </div>
     );
 };
---
+++ src/Pages/Home/Categories/Category.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Category = ({ category }) => {
+    const { name, img, info, _id } = category;
+    return (
+        <div className="card card-compact w-96  shadow-xl">
+            <figure><img src={img} alt="phones" /></figure>
+            <div className="card-body">
+                <h2 className="card-title">{name}</h2>
+                <p>{info}</p>
+                <div className="card-actions">
+                    <Link to={`/category/${_id}`} className='w-full'>
+                        <button className="btn btn-primary w-full">Buy Now</button>
+                    </Link>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Category;
